d9ab9d9361801021e7a0d6c187052043057665af
Keneth Ravindu <129732198+Keneth-Ravindu@users.noreply.github.com>
2025-03-04 02:28:31 +0530

    Continuation Commit

--- adminhome.php
+++ adminhome.php
@@ -12,8 +12,6 @@
     header("location:login.php");
     }
 
-
-
 ?>
 
 
@@ -22,11 +20,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="Styles/admin.css">
 </head>
 <body>
-    <h1>Admin Dashboard</h1>
 
-    <a href="logout.php">Logout</a>
+    <header class="header">
+        <a href="">Admin Dashboard</a>
+
+        <div class="logout">
+            <a href="" class="btn btn-primary">Logout</a>
+        </div>
+    </header>
+
+    <aside>
+        <ul>
+            <li>
+                <a href="">Admission</a>
+            </li>
+            <li>
+                <a href="">Add Student</a>
+            </li>
+            <li>
+                <a href="">View Student</a>
+            </li>
+            <li>
+                <a href="">Add Teacher</a>
+            </li>
+            <li>
+                <a href="">View Teacher</a>
+            </li>
+            
+        </ul>
+    </aside>
+
+
 </body>
 </html>
